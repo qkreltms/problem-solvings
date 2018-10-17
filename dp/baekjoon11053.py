4bafea69a68b48ec02ff8ff383c9765689d057f9
JungHoon Park <wfr112@naver.com>
2018-10-17 20:12:54 +0900

    Add baekjoon11053

---
+++ dp/baekjoon11053.py
@@ -0,0 +1,10 @@
+#https://www.acmicpc.net/problem/11053
+n = int(input())
+A = list(map(int, input().split()))
+d = [0] * (n + 1)
+for i in range(n):
+    d[i] = 1
+    for j in range(i):
+        if A[i] > A[j]:
+            d[i] = max(d[i], d[j]+1)
+print(max(d))
