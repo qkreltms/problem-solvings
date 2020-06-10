8ba5955dde7538223f8d1cd2c5e9f154d92fd3bf
qkreltms <junghooncentralpark@gmail.com>
2020-06-10 23:37:20 +0900

    update: baekjoon11724

--- graph/baekjoon11724.py
+++ graph/baekjoon11724.py
@@ -1,33 +1,37 @@
 # https://www.acmicpc.net/problem/11724
+# 참고 반례
+# 6 2
+# 1 3
+# 2 3
+# 이어지는 간선이 없다고 정점이 없는게 아닙니다
+# 그림으로 나타내면...
+# 1ㅡ2ㅡ3 4 5 6
+# 총 4개네요
+import sys
 
 def dfs(start):
   S = [start]
-  visited = [0] * (N+1)
 
   while S:
     V = S.pop()
     if visited[V] == 0:
       visited[V] = 1
       S += reversed(sorted(list(adj[V])))
-      del adj[V]
 
 # N = 정점의 개수, M = 간선의 개수
-N, M = map(int, input().split(" "))
+N, M = map(int, sys.stdin.readline().split(" "))
 
-adj = {i: set() for i in range(N+1)}
+adj = [[] for i in range(N+1)]
 
 for _ in range(M):
-  n1, n2 = map(int, input().split(" "))
-  adj[n1].add(n2)
-  adj[n2].add(n1)
+  n1, n2 = map(int, sys.stdin.readline().split(" "))
+  adj[n1].append(n2)
+  adj[n2].append(n1)
 
 component = 0
-while adj:
-  key = list(adj.keys())[0]
-  # 값이 아무것도 없을 때는 실행하지 않는다.
-  if len(adj[key]) == 0: 
-    del adj[key]
-    continue
-  dfs(key)
-  component +=1
+visited = [0] * (N+1)
+for i in range(1, N+1):
+  if visited[i] == 0:
+    dfs(i)
+    component +=1
 print(component, end="")
