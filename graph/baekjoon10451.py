9ef441ffadbb02b78bbe12993809904ca1387619
qkreltms <junghooncentralpark@gmail.com>
2020-07-27 21:17:37 +0900

    update: baekjoon10451

---
+++ graph/baekjoon10451.py
@@ -0,0 +1,31 @@
+# 2 몇 번 할지
+# 8 순열의 크기
+# 3 2 7 8 1 4 5 6 순열
+# 10
+# 2 1 3 4 5 6 7 9 10 8
+# 순열은 중복되는 수가 없는 나열된 수
+# 문제를 보면 방향이 있는 그래프이다.
+# 이 문제에서는 1->2 2->1 이 될 수는 있어도 1->2, 1->3과 같은 형식은 될 수 없다.
+
+import sys
+sys.setrecursionlimit(999999)
+def dfs(i):
+  if (visit[i] == False):
+    visit[i] = True
+    dfs(A[i])
+  else:
+    return
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  A = [0] + list(map(int, input().split(" ")))
+  visit = [False] * (n + 1)
+  
+  ans = 0
+  for i in range(1, n + 1):
+    if visit[i] == False:
+      dfs(i)
+      ans += 1
+  print(ans)
+
