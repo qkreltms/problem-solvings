20c691dc4362ea6509af91f65c0e69e5aefde4e1
qkreltms <junghooncentralpark@gmail.com>
2020-10-19 19:36:04 +0900

    update: baekjoon7576

--- graph/baekjoon7576/mine.py
+++ graph/baekjoon7576/mine.py
@@ -1,3 +1,4 @@
+# 토마토 알고리즘
 # 미로찾기 알고리즘에서 다른점은
 # 시작 포인트가 여러개다 => 그냥 시작할 때 queue에 여러 포인트를 넣는다.
 # 방문 못 하는 노드가 있을 때 예외 처리 => 방문 못 한 노드 있는지 확인
@@ -8,24 +9,34 @@
 # 1. 지도가 들어있는 값 순회 해서 1값을 찾고 찾은 값을 시작때 큐에 넣는다.
 # 2. visited에 -1, 못 간 구역 표시가 필요하다.
 # 3. 결과 값은 값 순회 한 후 최대거리 값을 출력한다. 또는 길찾기 알고리즘 실행시 판별해서 구함 
+import sys
+sys.setrecursionlimit(999999)
 
-w, h = map(int, input().split())
+w, h = map(int, sys.stdin.readline().split())
 nodes = [0 for _ in range(h)]
 for i in range(h):
-    nodes[i] = input().split()
+    nodes[i] = sys.stdin.readline().split()
 visited = [[-1 for _ in range(w)] for _ in range(h)]
+# 갈수있는 거리의 길이만 저장할 변수
+maxFootprints = w * h
 
 def getStartPoints():
+    global maxFootprints
     startPoints = []
     for i in range(h):
         for j in range(w):
             if nodes[i][j] == '1':
                 startPoints.append([i,j,-1])
-            elif nodes[i][j] == '-1':
-                visited[i][j] = -2
+            # 못 가는곳은 길이 제외한다.
+            if nodes[i][j] == '-1':
+                maxFootprints -= 1
     return startPoints
 
 def f():
+    global footPrints
+    global maxVisited
+    footPrints = 0
+    maxVisited = 0
     q = [*getStartPoints()]
     while q:
         y, x, v = q[0]
@@ -34,25 +45,21 @@ def f():
         if y >= 0 and x >= 0 and y < h and x < w:
             node = nodes[y][x] 
             if visited[y][x] == -1 and node != '-1':
+                footPrints += 1
                 visited[y][x] = v + 1
+                if maxVisited < visited[y][x]:
+                    maxVisited = visited[y][x]
                 q.append([y-1,x, visited[y][x]])
                 q.append([y,x+1, visited[y][x]])
                 q.append([y+1,x, visited[y][x]])
                 q.append([y,x-1, visited[y][x]])
 
 f()
-# find max
-max = 0
-flag = False
-for i in range(h):
-    for j in range(w):
-        if visited[i][j] == -1:
-            flag = True
-            break
-        if max < visited[i][j]:
-            max = visited[i][j]
-if flag == False:
-    print(max)
-else:
+# 갈수있는 총 0의 개수보다 적개 순회했다? => 못 간 0이 있다. => -1
+if footPrints != maxFootprints:
     print(-1)
+else :
+    print(maxVisited)
+
+
 
