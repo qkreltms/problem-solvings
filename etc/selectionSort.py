36484e39906c7c32b3c02a98210ea804aa76c885
qkreltms <junghooncentralpark@gmail.com>
2020-11-11 19:24:09 +0900

    update: 완주하지 못한 선수 2회차 클리어

--- etc/selectionSort.py
+++ etc/selectionSort.py
@@ -1,3 +1,4 @@
+# 배열을 순회하며, a, b(a보다 위의 인덱스를 가진 값) 중 어떤 값이 높은지 또는 낮은지를 비교해 swap해 정렬한다.
 def f(arr):
     for i in range(0, len(arr)-1):
         for j in range(i+1, len(arr)):
--- etc/완주하지 못한 선수/2회차.py
+++ etc/완주하지 못한 선수/2회차.py
@@ -1,3 +1,6 @@
+a = ['leo', 'eden', 'eden']
+b = 	['eden', 'leo']
+
 # 주의 python 3.8 이상은 sort 알고리즘 바뀐듯
 # x
 def solution(participant, completion):
@@ -10,4 +13,27 @@ def solution(participant, completion):
 
 print(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']))
 
-    
+# Counter 쓰기
+# o
+from collections import Counter
+
+def solution(a, b):
+  print(list(Counter(a) - Counter(b))[0])
+
+solution(a, b)  
+
+# hash 쓰기
+# x, 가장 느림
+def solution(a, b):
+  a.sort()
+  b.sort()
+  hashSum = 0 
+  dic = {}
+  for i in a:
+    myHash = hash(i)
+    hashSum += myHash
+    dic[myHash] = i
+  for j in b:
+    hashSum -= hash(j)
+  return dic[hashSum]
+print(solution(a, b))
