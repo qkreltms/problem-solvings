d3a851594be05661ab245633a7b6903e84f7f18f
qkreltms <junghooncentralpark@gmail.com>
2020-11-12 18:28:47 +0900

    update: 베스트 앨범

--- etc/베스트 앨범.py
+++ etc/베스트 앨범.py
@@ -1,30 +1,42 @@
 # HACK!!! python sort 알고리즘은 중복을 허용하지 않는다.
-# def solution(genres, plays):
-#   dic = {}
-#   agg = {}
-#   for i in range(len(genres)):
-#     dic[plays[i]] = i
-#     agg[plays[i]] = genres[i]
-#   sortedAgg = sorted(agg, reverse=True))
-#   visited = [False for _ in range(len(sortedAgg))]
+from collections import Counter
 
-#   i = 0
-#   ans = []
-#   for a in sortedAgg:
-#     if visited[dic[a]] == True:
-#       continue
-#     pivot = agg[a]
-#     appCnt = 0
-#     for j in range(i, len(agg)):
-#       if agg[sortedAgg[j]] == pivot:
-#         visited[dic[sortedAgg[j]]] = True
-#         ans.append(dic[sortedAgg[j]])
-#         appCnt += 1
-#     i+=1
-#     if appCnt > 2:
-#       for i in range(appCnt-2):
-#         ans.pop()
-#   return ans
+def solution(genres, plays):
+  visited = [False for _ in range(len(plays))]
+  
+  ans = []
+  genreCnt = len(sorted(list(Counter(genres))))
+
+  for c in range(genreCnt):
+    genreAns = [-1, -1]
+    max = 0
+    pivot = ''
+    for i in range(len(plays)): 
+      if visited[i] != True:
+        if plays[i] > max:
+          max = plays[i]
+          pivot = genres[i]
+          genreAns[0] = i
+
+    max = 0
+    visited[genreAns[0]] = True
+
+    for i in range(len(plays)): 
+      if visited[i] != True:
+        if pivot == genres[i]:
+          if plays[i] > max:
+            max = plays[i] 
+            visited[i] = True
+            genreAns[1] = i
+    if genreAns[1] == -1:
+      ans.append(genreAns[0])
+    else:
+      ans += genreAns
+  return ans
+      
+
 
 # # 답: 4, 2, 3, 장르별로 2개임!!
 # print(solution(['B', 'B', 'B', 'B', 'A'], [500, 150, 800, 800, 2500]))
+# print(solution(['A', 'B', 'A', 'A', 'B'], [500, 600, 150, 800, 2500]))
+print(solution(['B', 'A'], [500, 600]))
