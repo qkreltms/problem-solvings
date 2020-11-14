412f9b7612442bf129ea763ab548fa2044a2ddfd
qkreltms <junghooncentralpark@gmail.com>
2020-11-14 19:55:01 +0900

    add: 주식가격 1회차만에 클리어

---
+++ etc/주식가격/1회차.py
@@ -0,0 +1,34 @@
+'''
+1. 초 단위로 기록된 주식가격이 담긴 배열 prices가 주어진다.
+2. 가격이 떨어지지 않은 기간은 몇 초인지 반환하라.
+[1,2,3,2,3]일 때
+1초 시점의 1$는 배열을 순회했을 때 떨어진 적이 없다 => 배열 길이 (4)
+2초 시점의 2$는 끝까지 떨어지지 않았다. => 자신의 index - 배열 길이 (3)
+3초 시점의 3$는 배열을 순회했을 때 1초뒤 가격이 떨어졌다. => 1
+4초 시점의 2$는 1초간 가격이 떨어지지 않았다.
+5초 시점의 3$는 0초간 가격이 떨어지지 않았다.
+
+생각 1.
+Queue를 써서 앞 배열을 pop한 후 a에 저장, 배열을 순회한다. a > arr[i]일 경우  index값을 반환하고 처음부터 배열이 빌때까지 다시 진행.
+다 순회하고도 없으면 index 값 반환 
+'''
+# 1회차 클리어
+from collections import deque
+
+
+def solution(prices):
+    q = deque(prices)
+    ans = []
+    for i in range(len(prices)):
+        pivot = q.popleft()
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            cnt += 1
+            if pivot > prices[j]:
+                break
+
+        ans.append(cnt)
+    return ans
+
+
+print(solution([1, 2, 3, 2, 3]), [4, 3, 1, 1, 0])
