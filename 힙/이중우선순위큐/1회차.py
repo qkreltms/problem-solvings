60b7c9be395bf4bf4daa179236dc5d93ebb341f9
qkreltms <junghooncentralpark@gmail.com>
2020-12-06 16:35:08 +0900

    update: 이중우선순위큐 해결 완료

--- 힙/이중우선순위큐/1회차.py
+++ 힙/이중우선순위큐/1회차.py
@@ -1,4 +1,4 @@
-#문제
+# 문제
 '''
 return 연산 처리후 큐가 비어있으면 [0, 0], 비어있지 않으면 [최댓값, 최솟값]
 이중우선순위큐를 만든다.
@@ -10,7 +10,7 @@ D -1 => 큐에서최솟값 삭제
 단, 큐가 비어있을 때 삭제 연산 => 무시
 삭제 연산 일 때 최댓값 or 최솟값이 두개 라면 하나만 삭제.
 '''
-#풀이법
+# 풀이법
 '''
 1트
 추가되는 값들은 그냥 배열에 저장해 놓는다.
@@ -20,7 +20,7 @@ D -1 => 큐에서최솟값 삭제
 => heapify 가 log(n)
 
 2트
-값을 먼저 다 받은 후 그 값을 
+값을 먼저 다 받은 후 그 값을
 내림차순 정렬한 배열을(A) 하나 만든다
 min heap을 하나 만든다.
 visited = []
@@ -35,14 +35,59 @@ visited = []
 => 삭제 명령어가 사이사이마다 들어오는데, 3트 방법 대로면 좀 까다로워짐
 
 4트 2트 방법 수정
+1. max heap, min heap, 이 둘을 동기화 시켜줄 배열 A를 만든다.
+2. 두 heap, A에 값을 잘 넣고, 삭제 명령어가 떨어지면 A에 값이 있는지 확인 후
+값 삭제, A에 해당 값도 삭제 => max, min heap에 A에 없는 값이 계속 남아있어 영향을 줌
 
+다만, 위 방법은 n2이 소요됨 heap을 삭제, 추가 해도 결국 nlogn정도 밖에 들지 않으니까
+그냥 어떤 heap에 삭제가 일어나면 반대쪽 쪽힙에는 그 값을 찾을 때까지 pop한 후
+그 값을 찾으면 그 값 삭제하고 나머지 값들은 다시 넣어주면 되지 않을까?
+=> 아래 코드와 같이 위와 비슷하지만 좀 다른 식으로 풀었음, 다만 O(n2)이라 성능 걱정을 했지만 통과함 
 '''
+
+
+
+
 import heapq
 def solution(ops):
-    heap = []
-    tail = 0
-    head = 0
+    ops.append("D -1")
+    ops.append("D 1")
+    maxHeap = []
+    minHeap = []
+    A = []
+    ans = [0, 0]
     for op in ops:
-        op = op.split()
-        if op[i] == "I":
-        if op[i] == "D":
+        # 추가
+        if op[0] == "I":
+            value = int(op[2:])
+            heapq.heappush(maxHeap, -value)
+            heapq.heappush(minHeap, value)
+        # 삭제
+        if op[0] == "D":
+            # min heap 삭제
+            if op[2] == '-':
+                if minHeap:
+                    minValue = heapq.heappop(minHeap)
+                    maxHeap.pop(maxHeap.index(-minValue))
+                    ans[1] = minValue
+                else:
+                    ans[1] = 0
+
+            # max heap 삭제
+            else:
+                if maxHeap:
+                    maxValue = heapq.heappop(maxHeap)
+                    minHeap.pop(minHeap.index(-maxValue))
+                    ans[0] = -maxValue
+                else:
+                    ans[0] = 0
+    return ans
+
+
+print(solution(["I 16", "D 1"]), [0, 0])
+print(solution(["I 7", "I 5", "I -5", "D -1"]), [7, 5])
+print(solution(["I -5"]), [0, -5])
+print(solution(	["I 16", "I -5643", "D -1",
+                 "D 1", "D 1", "I 123", "D -1"]), [0, 0])
+print(solution(	["I -45", "I 653", "D 1", "I -642",
+                 "I 45", "I 97", "D 1", "D -1", "I 333"]), [333, -45])
