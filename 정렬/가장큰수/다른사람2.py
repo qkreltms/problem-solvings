583f9ab0d11c7f28e124a08221a05addf88e2f1f
qkreltms <junghooncentralpark@gmail.com>
2020-12-09 21:50:59 +0900

    update: H-index 생각 중...

--- 정렬/가장큰수/다른사람2.py
+++ 정렬/가장큰수/다른사람2.py
@@ -3,10 +3,13 @@ import functools
 def comparator(a,b):
     t1 = a+b
     t2 = b+a
-    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+    print(a,b,t1,t2) 
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0 // 1이면 a가 왼쪽 이동, -1이면 b가 왼쪽 이동
 
 def solution(numbers):
     n = [str(x) for x in numbers]
     n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
     answer = str(int(''.join(n)))
     return answer
+
+print(solution([6, 10, 11]))
