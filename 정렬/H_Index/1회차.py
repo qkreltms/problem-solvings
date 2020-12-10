5fdf1be36b9a2eaf3460b457dce085859785bd86
qkreltms <junghooncentralpark@gmail.com>
2020-12-10 23:24:21 +0900

    update: H-index 1트 성공

--- 정렬/H_Index/1회차.py
+++ 정렬/H_Index/1회차.py
@@ -1,14 +1,37 @@
-#문제
+# 문제
 '''
 '''
-#키 포인트
+# 키 포인트
 '''
 
 '''
-#풀이
+# 풀이
 '''
+#1
 1. 모든 조합을 비교
 2. 작은 값이 대체로 결과 값이 됨 => 오름차순 정렬
 3. 0~n 번째 순으로 pick하고 배열을 돌면서 
-이상, 이하 개수 구하고 pick 한 숫자와 값이 같으면 return
+이상 개수 구하고 pick 한 숫자와 값이 같으면 return
 '''
+
+
+def solution(N):
+    N.sort()
+    l = len(N)
+    cnts = []
+    for pick in range(l+1):
+        cnt = 0
+        for j in N:
+            if pick <= j:
+                cnt += 1
+        if pick > cnt:
+            break
+        cnts.append((pick, cnt))
+    return max(cnts, key=lambda x:x[0])[0]
+
+
+print(solution([3, 0, 6, 1, 5]), 3)
+print(solution([0, 0, 0, 0, 0]), 0)
+print(solution([0, 0, 0, 0, 2, 3]), 2)
+print(solution([0, 0, 0, 3, 5, 5, 6, 7, 7, 8]), 5)
+print(solution([350, 452, 877]), 3)
---
+++ 정렬/H_Index/다른사람.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
