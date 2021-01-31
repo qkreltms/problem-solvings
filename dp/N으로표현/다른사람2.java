42433a52e28ab3cc45e9ac05fb1a3947f41101d5
qkreltms <junghooncentralpark@gmail.com>
2021-01-31 23:35:31 +0900

    update: 정수삼각형 다른사람 코드 리뷰

--- dp/N으로표현/다른사람2.java
+++
@@ -1,3 +0,0 @@
-'''
-https://ydeer.tistory.com/42
-'''
--- dp/정수삼각형/다른사람2.py
+++ dp/정수삼각형/다른사람2.py
@@ -1,3 +1,8 @@
+'''
+바텀업 형식으로 진행, 부모에 자식 중 가장 큰 값을 부모 값과 더해서 넣음
+'''
+
+
 def solution(triangle):
     memo = {}
     answer = f(triangle, 0, 0, memo)
@@ -18,3 +23,7 @@ def f(triangle, i, j, memo):
     memo[(i, j)] = x
 
     return x
+
+
+print(solution([[0], [1, 0], [1, 0, 0]]), 2)
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]), 30)
