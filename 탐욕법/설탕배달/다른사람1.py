c6e71a3755350dd55aaaae024ed97c7525cce53d
qkreltms <junghooncentralpark@gmail.com>
2021-02-08 23:46:59 +0900

    update: 설탕배달 리뷰

--- 탐욕법/설탕배달/다른사람1.py
+++ 탐욕법/설탕배달/다른사람1.py
@@ -1,17 +1,23 @@
-def algoritm2839():
-    i = int(input())
-    n = i
-    bag5 = int(n / 5)
-    n = n % 5
-    while (bag5 >= 0):
-        if (n % 3 == 0 or n == 0):
-            bag3 = int(n / 3)
-            return bag3 + bag5
-        if (bag5 == 1):
-            n = bag5 * 5 + n
-            bag5 -= 1
-        else:
-            bag5 -= 1
-            n = i - bag5 * 5
-    return -1
-print(algoritm2839())
+'''
+'''
+#include <stdio.h>
+void main() {
+    int Box = 0, input;
+    scanf("%d", &input);
+ 
+    while (1) {
+        if (input % 5 == 0) {
+            Box += input / 5;
+            printf("%d", Box);
+            break;
+        }
+ 
+        input = input - 3;
+        Box++;
+ 
+        if (input < 0) {
+            printf("-1");
+            break;
+        }
+    }
+}
