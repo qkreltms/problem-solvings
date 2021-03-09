99e5b71414d26c0111e41e2098f15e7e61605f38
qkreltms <junghooncentralpark@gmail.com>
2021-03-09 20:34:16 +0900

    update: 입국심사 1회차 완료

--- 이분탐색/이분탐색/입국심사/1회차.py
+++ 이분탐색/이분탐색/입국심사/1회차.py
@@ -12,5 +12,36 @@
 '''
 # 풀이방법
 '''
+심사관이 처리가능한 최소, 최대 범위를 구하고
+대략 중간 값으로 심사관 모두의 처리량을 구한다.
+만약 처리량이 n보다 낮으면
+start 를 중간+1으로 옮기고 다시 한다.(작업량을 더 부여한다.) (loop)
+만약 처리량이 n보다 높거나 같으면
+end를 mid-1로 두어 작업을 계속한다.(최적 값을 찾기위해 작업량을 줄여본다.)
+이렇게 작업량을 조절하며 범위를 좁히다보면 결과가 나온다.
+만약 start <= end가 되면 작업을 종료하고 처리 결과를 반환한다. 
 '''
+
+
 def solution(n, times):
+    fastestTime = min(times)
+    start = fastestTime
+    end = fastestTime*n
+
+    ans = []
+    while start <= end:
+      mid = (start+end)//2
+      sumTasks = 0
+      for t in times:
+        sumTasks += mid//t
+        if n <= sumTasks:
+          end = mid-1
+          ans.append(mid)
+          break
+      if n > sumTasks:
+        start = mid+1
+    return min(ans)
+  
+print(solution(6, [7,10]), 28)
+print(solution(1, [1]), 1)
+print(solution(3, [1, 2]), 2)
