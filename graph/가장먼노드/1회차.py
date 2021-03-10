6b34edce55bc70874d9f4a139ddb63b26653d761
qkreltms <junghooncentralpark@gmail.com>
2021-03-10 20:39:09 +0900

    update: 가장먼노드 1회차 완료

--- graph/가장먼노드/1회차.py
+++ graph/가장먼노드/1회차.py
@@ -5,33 +5,48 @@
 노드의 개수는? 
 '''
 
-
-def solution(n, edge):
-    visited = [False for _ in range(n+1)]
-    for e in edge:
-        e.sort()
-    dic = {}
+# 풀이법
+'''
+처음에 
     for key, value in edge:
         if key in dic:
             dic[key].append(value)
         else:
             dic[key] = [value]
+와 같이 담았을 때 
+1-2, 3-1 과 같은 입력일 때 3은 영영 담기지 않는다.
+결국 양 노드에 값을 똑같이 줌으로 해결
+* 1-2, 1-3 과 같이 root-child로 들어오는 순이아니라면 양 노드에 담아주어야 한다.
+
+    for v in reversed(visited):
+        if v != m:
+            return cnt
+와 같이 구현했을 때
+항상 마지막에 위치한 값만 max가 되지 않기 때문에 실패
+'''
+
+
+
+def solution(n, edge):
+    visited = [False for _ in range(n+1)]
+    dic = [[] for _ in range(n+1)]
+    for a, b in edge:
+      dic[a].append(b)
+      dic[b].append(a)
 
     queue = [[1, 1]]
     while queue:
         q, level = queue.pop(0)
         if visited[q] == False:
             visited[q] = level
-            if q in dic:
-                nodes = dic[q]
-                for n in nodes:
-                    queue.append([n, level+1])
+            for n in dic[q]:
+                queue.append([n, level+1])
     m = max(visited)
     cnt = 0
-    for v in reversed(visited):
-        if v != m:
-            return cnt
-        cnt += 1
+    for v in visited:
+        if v == m:
+            cnt += 1
+    return cnt
 
 
 print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]), 3)
@@ -40,3 +55,7 @@ print(solution(11, [[1, 2], [1, 3], [2, 4], [2, 5], [3, 5], [
 print(solution(4, [[1, 2], [2, 3], [3, 4]]), 1)
 print(solution(2, [[1, 2]]), 1)
 print(solution(5, [[4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]), 2)
+print(solution(4, [[1, 2], [1, 3], [2, 3], [2, 4], [3, 4]]), 1)
+print(solution(4, [[1, 4], [1, 3], [2, 3], [2, 1]]), 3)
+print(solution(4, [[3, 4], [1, 3], [2, 3], [2, 1]]), 1)
+print(solution(4, [[4, 3], [1, 3], [2, 3]]), 2)
---
+++ graph/가장먼노드/다른사람.py
@@ -0,0 +1,23 @@
+def solution(n, edge):
+    graph =[  [] for _ in range(n + 1) ]
+    distances = [ 0 for _ in range(n) ]
+    is_visit = [False for _ in range(n)]
+    queue = [0]
+    is_visit[0] = True
+    for (a, b) in edge:
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
+
+    while queue:
+        i = queue.pop(0)
+
+        for j in graph[i]:
+            if is_visit[j] == False:
+                is_visit[j] = True
+                queue.append(j)
+                distances[j] = distances[i] + 1
+
+    distances.sort(reverse=True)
+    answer = distances.count(distances[0])
+
+    return answer
