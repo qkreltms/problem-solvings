a32dcdb1f5db0e48aa0982d2d3b94ba39f7ed12e
qkreltms <junghooncentralpark@gmail.com>
2021-03-15 21:05:34 +0900

    update: 문자열압축 2일차 푸는 중

--- 문자열/문자열압축kakao2020/1회차.py
+++ 문자열/문자열압축kakao2020/1회차.py
@@ -14,3 +14,34 @@ abcabc abcabc dedede dedede
 '''
 1, ..., S//2개수 만큼 앞에서부터 묶어 최적화한 값을 results에 넣고 최소 len 값을 반환한다.
 '''
+
+
+def solution(S):
+    ans = len(S)
+    for r in range(1, len(S)//2+1):
+        tokens = []
+        T, i = S, 0
+        while T:
+            tokens.append([T[0:r]])
+            T = T[r:]
+            i += 1
+        tokens.append('!')  # end
+
+        result = ''
+        while i > len(tokens):
+            j = i+1
+            cnt = 1
+            while tokens[i] == tokens[j]:
+                j += 1
+                cnt += 1
+            else:
+                if cnt > 2:
+                    result += str(cnt) + tokens[i]
+                    i = j
+                else:
+                    result += tokens[i]
+        ans = min(ans, len(result))
+    return ans
+
+
+print(solution('aabbcc'))
