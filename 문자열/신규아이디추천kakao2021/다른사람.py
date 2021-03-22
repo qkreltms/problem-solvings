81e9a5f96e933e8148c9c652f75a518b40d06a35
qkreltms <junghooncentralpark@gmail.com>
2021-03-22 21:15:12 +0900

    update: 신규아이디추천 다른 사람 코드 리뷰 완료

--- 문자열/신규아이디추천kakao2021/다른사람.py
+++ 문자열/신규아이디추천kakao2021/다른사람.py
@@ -3,8 +3,11 @@ import re
 def solution(new_id):
     st = new_id
     st = st.lower()
+    # sub => 해당되는 정규식 어떤 값으로 변환해 반환
     st = re.sub('[^a-z0-9\-_.]', '', st)
+    # .이 여러개 있으면
     st = re.sub('\.+', '.', st)
+    # .으로 시작하거나 끝나면
     st = re.sub('^[.]|[.]$', '', st)
     st = 'a' if len(st) == 0 else st[:15]
     st = re.sub('^[.]|[.]$', '', st)
