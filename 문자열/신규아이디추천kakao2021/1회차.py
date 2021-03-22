6b5cd4585b239848a3c22cad9c63059996a9503f
qkreltms <junghooncentralpark@gmail.com>
2021-03-22 21:12:09 +0900

    update: 신규아이디추천 1회차 완료

--- 문자열/신규아이디추천kakao2021/1회차.py
+++ 문자열/신규아이디추천kakao2021/1회차.py
@@ -15,3 +15,48 @@
      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 '''
+# 풀이 법
+'''
+'''
+import re
+def solution(newId):
+  recommId = newId
+  recommId = newId.lower()
+  p = re.compile('[a-z0-9-_.]')
+  temp = ''
+  for s in recommId:
+    if (p.match(s)):
+      temp += s
+  recommId = temp
+
+  while True:
+    i = recommId.find('..')
+    if i == -1:
+      break  
+    recommId = recommId[0:i] + recommId[i+1:]
+  
+  if recommId and recommId[0] == '.':
+    recommId = recommId[1:]
+  if recommId and recommId[-1] == '.':
+    recommId = recommId[0:-1]
+
+  if not recommId:
+    recommId = 'a'
+  
+  if len(recommId) >= 16:
+    recommId = recommId[0:15]
+    if recommId[-1] == '.':
+      recommId = recommId[0:-1]
+  
+  if len(recommId) <= 2:
+    c = recommId[-1]
+    while len(recommId) < 3:
+      recommId += c
+  return recommId 
+
+print(solution("...!@BaT#*..y.abcdefghijklm"), "bat.y.abcdefghi")
+print(solution("z-+.^."), "z--")
+print(solution("=.="), "aaa")
+print(solution("...."), "aaa")
+print(solution("123_.def"), "123_.def")
+print(solution("abcdefghijklmn.p"), "abcdefghijklmn")
