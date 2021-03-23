b974c0943e1fb8ba9c66783731473df8c02c0a95
qkreltms <junghooncentralpark@gmail.com>
2021-03-23 21:07:22 +0900

    update: 구름 셀프레벨테스트 9차 진행중...

---
+++ 구름/앵무새꼬꼬/1회차.py
@@ -0,0 +1,19 @@
+'''
+문자열이 주어지면 모음(aeiou)만 반환하시오,
+단 모음이 없으면 !!!
+'''
+
+import re
+def f(param):
+  p = re.compile('[^aeiou]', re.IGNORECASE)
+  return p.sub('', param) or '???'
+
+# n = int(input())
+# for _ in range(n):
+#   param = input()
+#   print(f(param))
+
+
+print(f('Hello, World!'))
+print(f("I'm your father."))
+print(f("What are you doing here?"))
