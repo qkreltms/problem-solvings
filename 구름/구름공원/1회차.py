43afee170d0c880494ed6d7d7bc7dd4af9f17270
qkreltms <junghooncentralpark@gmail.com>
2021-04-23 16:12:24 +0900

    update: 카테고리화

--- 구름/구름공원/1회차.py
+++
@@ -1,3 +0,0 @@
-'''
-https://level.goorm.io/exam/58258/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%9E%88%EC%96%B4%EB%A1%9C%EC%A6%88-%EC%85%80%ED%94%84%EB%A0%88%EB%B2%A8%ED%85%8C%EC%8A%A4%ED%8A%B8-9%EC%B0%A8/quiz/3
-'''
