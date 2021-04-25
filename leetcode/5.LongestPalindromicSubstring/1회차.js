c1570b75789106cc461816967797df542a770aaa
qkreltms <junghooncentralpark@gmail.com>
2021-04-25 18:40:04 +0900

    update: leetcode 5번 진행중

--- leetcode/5.LongestPalindromicSubstring/1회차.js
+++ leetcode/5.LongestPalindromicSubstring/1회차.js
@@ -1,30 +1,59 @@
+//링크
+/*
+문제: https://leetcode.com/problems/longest-palindromic-substring/
+가장 긴 팰린드롬(뒤집어도 같음 예: 토마토)을 주어진 문자열의 부분문자열에서 찾아라
+참고:
+*/
 
+//풀이 법
+/*
+bruteforce 방식
+문자열을 순회하며 모든 부분문자열을 가져온다.
+만약 h에 없는 문자열이라면 h에 넣고
+팰린드롬인지 판별한다.
+만약 h에 있다면 스킵하고 다음 부분문자열을 찾는다.
+*/
 
-/**
- * @param {string} s
- * @return {string}
- */
- var longestPalindrome = function(s) {
-   function isPalindrome(s) {
-     const target = s.split('').reverse().join('')
-     if (s === target) {
-       return true
-     }
-     return false
-   }
+//키포인트
+/*
+성능 최적화
+문자열길이가 최대 1000개 
+bruteforce 방법은 n^3, 최대 n^2으로 줄여야함
+*/
 
-   const results = {}
-   for(let i=0; i<s.length; i++) {
-     for(let j=j; j<s.length+1; j++) {
-       const subString = s.slice(i, j)
-       if(isPalindrome(subString)) {
-         results[subString] = subString.length
-       }
-     }
-   }
+const isP = (S) => {
+  const r = S.split('').reverse().join('')
+  if (r === S) {
+    return true
+  }
+  return false
+}
+function splice(S, i, j) {
+  return S.split('').splice(i, j + 1 - i).join('')
+}
+var longestPalindrome = function (S) {
+  const h = {}
+  const l = S.length
+  const results = []
+  for (let i = 0; i < l; i++) {
+    for (let j = i; j < l; j++) {
+      const s = splice(S, i, j)
+      if (h[s]) {
+        continue;
+      }
+      h[s] = s
+      if (isP(s)) {
+        results.push(s)
+      }
+    }
+  }
 
-  //  return Object.entries(results).max()
+  results.sort((a, b) => b.length - a.length)
+  ans = results[0]
+  return ans
 };
 
-// longestPalindrome('abhcchchh')
-longestPalindrome('abba')
+
+// longestPalindrome('abba')
+// longestPalindrome("abcdbbfcba")
+longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")
--- leetcode/5.LongestPalindromicSubstring/다른사람.py
+++ leetcode/5.LongestPalindromicSubstring/다른사람.py
@@ -1,3 +1,47 @@
+# 링크
 '''
-https://zkf85.github.io/2019/03/26/leetcode-005-longest-palindrome
+문제:
+참고: https://leetcode.com/problems/longest-palindromic-substring/discuss/2928/Very-simple-clean-java-solution
 '''
+# 풀이 법
+'''
+'''
+# 키포인트
+'''
+'''
+
+
+class Solution1(object):
+
+    def __init__(self):
+        self.lo = 0
+        # 0보다 1로하면 속도가 더 빨리짐 왜?
+        self.maxlen = 1
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        for i in range(len(s)):
+            self.__extendPalindrome(s, i, i) # 홀수
+            self.__extendPalindrome(s, i, i+1) # 짝수
+        return s[self.lo: self.lo + self.maxlen]
+
+    def __extendPalindrome(self, s, left, right):
+        while(left >= 0 and right < len(s) and s[left] == s[right]):
+            left -= 1
+            right += 1
+            # 최대 길이를 구하는 것이므로 최대 길이 넘는 것만
+        if self.maxlen < right -1 - left:
+            self.lo = left + 1  # left+1:  our l-- will go  unequal OR off bounds
+            # 만약 addbab, i=4, l=2, r=6 일 때 r이 한칸 더 넘어갔으므로 -1해주고 l만큼의 공간을 r에서 빼면 
+            # 길이 값이 나온다.  
+            self.maxlen = right -1 - left # right-1:  our r++ will go  unequal OR off bounds, python에 맞춘 식
+
+
+# Solution1().longestPalindrome('babad')
+# print(Solution1().longestPalindrome('abefba'))
+print(Solution1().longestPalindrome('cbabd'))
+# print(Solution1().longestPalindrome('babad'))
+# print(Solution1().longestPalindrome('adbab'))
