e6fed191abaedc7bec5ab1dbb434bae765c5bc40
qkreltms <junghooncentralpark@gmail.com>
2021-06-11 09:24:18 +0900

    update: 동전1 2회차 완료

---
+++ BOJ/동전1/2회차-dp.py
@@ -0,0 +1,8 @@
+n,k=map(int,input().split())
+coins=[int(input()) for _ in range(n)]
+dp=[0 for _ in range(k+1)]
+dp[0]=1
+for c in coins:
+    for i in range(c,k+1):
+        dp[i]+=dp[i-c]
+print(dp[k])
