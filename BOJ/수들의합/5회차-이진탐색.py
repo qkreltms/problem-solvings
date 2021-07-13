1edd4d9fbb0dd4fe04daa5aa15a843543fb6a864
qkreltms <junghooncentralpark@gmail.com>
2021-07-13 15:42:25 +0900

    feat: 수들의합 복습 완료

---
+++ BOJ/수들의합/5회차-이진탐색.py
@@ -0,0 +1,14 @@
+s=int(input())
+l,r=0,s
+m=s//2
+ans=0
+while l<=r:
+    mid=(l+r)//2
+    hap=mid*(mid+1)//2
+    if hap>s:
+        r=mid-1
+    else:
+        l=mid+1
+        ans=mid
+print(ans)
+    
